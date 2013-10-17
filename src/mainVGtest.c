f5224222ff4455b493328d2cf05987278588455b
jargon777 <matt_muresan@hotmail.com>
2013-10-17 15:55:04 +0000

    Initial test commit and test C file.
    Contains the openvg api by Anthony Starks, as well as a test of this implementation.
    
    Final commit before rebasing and using branches.

---
+++ src/mainVGtest.c
@@ -0,0 +1,39 @@
+/*
+ * main.c 
+ * 
+ * Program contains main methods for RasPi operations. At the moment this is nothing but a testing ground.
+ * 
+ * Depends on: libjpeg8-dev, and an openVG api included in this set: git://github.com/ajstarks/openvg
+ * Compile: gcc -Wall -I/home/pi/openvg -I/home/pi/projects/RPi-control -I/opt/vc/include -I/opt/vc/include/interface/vcos/pthreads -c "%f" ../lib/openvg/libshapes.o ../lib/openvg/oglinit.o -L/opt/vc/lib -lGLESv2 -ljpeg
+ * Build: gcc -Wall -I/home/pi/openvg -I/home/pi/projects/RPi-control -I/opt/vc/include -I/opt/vc/include/interface/vcos/pthreads -o "%e" "%f" ../lib/openvg/libshapes.o ../lib/openvg/oglinit.o -L/opt/vc/lib -lGLESv2 -ljpeg
+ * 
+ */
+
+// first OpenVG program 
+// Anthony Starks (ajstarks@gmail.com)
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "VG/openvg.h"
+#include "VG/vgu.h"
+#include "fontinfo.h"
+#include "shapes.h"
+
+int main() {
+    int width, height;
+    char s[3];
+
+    init(&width, &height);                  // Graphics initialization
+
+    Start(width, height);                   // Start the picture
+    Background(0, 0, 0);                    // Black background
+    Fill(44, 77, 232, 1);                   // Big blue marble
+    Circle(width / 2, 0, width);            // The "world"
+    Fill(255, 255, 255, 1);                 // White text
+    TextMid(width / 2, height / 2, "hello, world", SerifTypeface, width / 10);  // Greetings 
+    End();                                  // End the picture
+
+    fgets(s, 2, stdin);                     // look at the pic, end with [RETURN]
+    finish();                               // Graphics cleanup
+    exit(0);
+}
